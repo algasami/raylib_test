ab1e30e870d9e549fac695b377a8bcbcc01d79dc
algasami <105358814+algasami@users.noreply.github.com>
2024-03-30 16:00:40 +0800

    terrain generation test

--- core_basic_window.c
+++ core_basic_window.c
@@ -4,17 +4,23 @@
 
 #include "raylib.h"
 #include "raymath.h"
+#include <stdint.h>
+#include <stdio.h>
 
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
 #endif
 
-int screenWidth = 800;
-int screenHeight = 450;
-Camera3D camera = {0};
+#define NOISE_ROW 30
+#define NOISE_COL 30
+
+uint32_t screenWidth = 1200;
+uint32_t screenHeight = 1200;
+
+Camera3D camera;
+
 Mesh cubeMesh;
-Model cubeModel;
-Vector3 cubePos = {0.0, 0.0, 0.0};
+Model cubeModels[NOISE_ROW][NOISE_COL];
 
 void UpdateDrawFrame();
 int main() {
@@ -22,20 +28,25 @@ int main() {
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "basic window");
 
-    camera.position = (Vector3){10.0f, 10.0f, 10.0f}; // Camera position
-    camera.target = (Vector3){0.0f, 0.0f, 0.0f};      // Camera looking at point
+    camera.fovy = 45.0f;
+    camera.position = (Vector3){1.0f, 10.0f, 1.0f};
+    camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+    camera.projection = CAMERA_PERSPECTIVE;
     camera.up = (Vector3){0.0f, 1.0f, 0.0f};
-    camera.fovy = 45.0f;                    // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE; // Camera projection type
 
-    cubeMesh = GenMeshCube(2.0, 4.0, 2.0);
-    cubeModel = LoadModelFromMesh(cubeMesh);
+    cubeMesh = GenMeshCube(1.0f, 5.0f, 1.0f);
+
+    for (size_t i = 0; i < NOISE_ROW; i++)
+        for (size_t j = 0; j < NOISE_COL; j++) {
+            cubeModels[i][j] = LoadModelFromMesh(cubeMesh);
+        }
+
+    EnableCursor();
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
 #else
-    SetTargetFPS(60);
-    DisableCursor();
+    SetTargetFPS(144);
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
@@ -47,23 +58,36 @@ int main() {
 }
 
 void UpdateDrawFrame() {
-    const float dt = GetFrameTime();
-    UpdateCamera(&camera, CAMERA_FREE);
-    cubeModel.transform =
-        MatrixMultiply(MatrixRotateX(dt * 2 * PI / 180), cubeModel.transform);
+    float dt = GetFrameTime();
+    float ft = (float)GetTime();
+
+    if (IsCursorHidden())
+        UpdateCamera(&camera, CAMERA_FREE);
+
+    Image noiseImage = GenImagePerlinNoise(NOISE_COL, NOISE_ROW,
+                                           (uint32_t)(ft * 20.0f), 0, 1.0f);
+
     ClearBackground(BLACK);
+
+    if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+        DisableCursor();
+    } else if (IsMouseButtonReleased(MOUSE_RIGHT_BUTTON)) {
+        EnableCursor();
+    }
 
     // start command buffer recording
     BeginDrawing();
     {
-        // start 3D mode recording
         BeginMode3D(camera);
-        DrawModel(cubeModel, cubePos, 1.0, RED);
-        DrawModelWires(cubeModel, cubePos, 1.0, BLACK);
-
-        // stop 3D mode recording
+        for (size_t i = 0; i < NOISE_ROW; i++)
+            for (size_t j = 0; j < NOISE_COL; j++) {
+                Color colorData = ((Color *)noiseImage.data)[i * NOISE_COL + j];
+                float height = (float)colorData.r / 255.0f;
+                DrawModel(cubeModels[i][j],
+                          (Vector3){(float)i, height * 10.0f, (float)j}, 1.0f,
+                          ColorFromHSV(170.0f, 1.0f, height));
+            }
         EndMode3D();
-
         DrawFPS(0, 0);
     }
     // stop command buffer recording
