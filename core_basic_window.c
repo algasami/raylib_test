4dc9b45111ec9172c5dcb4e23c6dc70d49629a7e
algasami <105358814+algasami@users.noreply.github.com>
2024-03-30 08:54:17 +0800

    REVAMP architecture

--- core_basic_window.c
+++ core_basic_window.c
@@ -11,53 +11,61 @@
 
 int screenWidth = 800;
 int screenHeight = 450;
+Camera3D camera = {0};
+Mesh cubeMesh;
+Model cubeModel;
+Vector3 cubePos = {0.0, 0.0, 0.0};
 
+void UpdateDrawFrame();
 int main() {
     // Initialization
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "basic window");
 
-#if defined(PLATFORM_WEB)
-    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
-#else
-    SetTargetFPS(60);
-
-    Camera3D camera = {0};
     camera.position = (Vector3){10.0f, 10.0f, 10.0f}; // Camera position
     camera.target = (Vector3){0.0f, 0.0f, 0.0f};      // Camera looking at point
     camera.up = (Vector3){0.0f, 1.0f, 0.0f};
     camera.fovy = 45.0f;                    // Camera field-of-view Y
     camera.projection = CAMERA_PERSPECTIVE; // Camera projection type
 
-    Mesh cubeMesh = GenMeshCube(2.0, 4.0, 2.0);
-    Model cubeModel = LoadModelFromMesh(cubeMesh);
-    Vector3 cubePos = {0.0, 0.0, 0.0};
+    cubeMesh = GenMeshCube(2.0, 4.0, 2.0);
+    cubeModel = LoadModelFromMesh(cubeMesh);
 
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+    SetTargetFPS(60);
     DisableCursor();
-
     // Main game loop
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-        UpdateCamera(&camera, CAMERA_FREE);
-        cubeModel.transform =
-            MatrixMultiply(MatrixRotateX(2 * PI / 180), cubeModel.transform);
-        ClearBackground(BLACK);
-
-        if (IsKeyPressed(KEY_Q)) {
-            break;
-        }
-
-        BeginDrawing();
-        {
-            BeginMode3D(camera);
-            DrawModel(cubeModel, cubePos, 1.0, RED);
-            DrawModelWires(cubeModel, cubePos, 1.0, BLACK);
-            EndMode3D();
-        }
-        EndDrawing();
+        UpdateDrawFrame();
     }
 #endif
-
     CloseWindow();
-
     return 0;
+}
+
+void UpdateDrawFrame() {
+    const float dt = GetFrameTime();
+    UpdateCamera(&camera, CAMERA_FREE);
+    cubeModel.transform =
+        MatrixMultiply(MatrixRotateX(dt * 2 * PI / 180), cubeModel.transform);
+    ClearBackground(BLACK);
+
+    // start command buffer recording
+    BeginDrawing();
+    {
+        // start 3D mode recording
+        BeginMode3D(camera);
+        DrawModel(cubeModel, cubePos, 1.0, RED);
+        DrawModelWires(cubeModel, cubePos, 1.0, BLACK);
+
+        // stop 3D mode recording
+        EndMode3D();
+
+        DrawFPS(0, 0);
+    }
+    // stop command buffer recording
+    EndDrawing();
 }
