d10e8600384732e9bf385c469205d898d5401179
algasami <105358814+algasami@users.noreply.github.com>
2024-06-19 12:08:55 +0800

    added graph visualizer

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # Raylib Test
 
+## Target: gravity_sim
+
 This is a simple gravitational field simulation
 that does not take general relativity into account.
 This simulation is capable of tracking _**3,000
 rigib bodies**_ with their own gravitational field
 at _**130~ FPS**_.
 
-## Keybindings
+### Keybindings
 
-- `T`: Slow motion. This decreases the error term O(dt*dt)
-by 2,500 times (since I use verlet integration.)
+- `T`: Slow motion. This decreases the error term O(dt\*dt)
+  by 2,500 times (since I use verlet integration.)
 - `S`: Spawn a rigid body with stochastic mass and radius.
+
+## Target: graph
+
+This is a simple graph visualizer built on top of Boost Graph
+Library (BGL). Clicking on nodes enables "connection mode", when
+you can click on another node to connect them together.
+
+### Keybindings
+
+- `T`: Zap the camera toward the first vertex.
--- src/graph.cpp
+++ src/graph.cpp
@@ -6,9 +6,17 @@
 
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_utility.hpp>
+#include <boost/graph/kamada_kawai_spring_layout.hpp>
+#include <boost/graph/random_layout.hpp>
+#include <boost/graph/topology.hpp>
+#include <random>
 
 constexpr unsigned int SCREEN_WIDTH = 1500;
 constexpr unsigned int SCREEN_HEIGHT = 1000;
+
+constexpr unsigned int MAX_NODES = 1000;
+
+constexpr float CIRCLE_RADIUS = 20.0f;
 
 Camera2D camera;
 
@@ -16,25 +24,36 @@ struct Vec {
     int key;
 };
 struct Edge {
-    int weight;
+    double weight;
 };
 
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, Vec, Edge> SimpleGraph;
+typedef boost::adjacency_list<boost::listS, boost::vecS, boost::undirectedS, Vec, Edge> simplegraph_t;
 
-SimpleGraph graph;
+typedef boost::graph_traits<simplegraph_t>::vertex_descriptor vertex_t;
+typedef boost::graph_traits<simplegraph_t>::edge_descriptor edge_t;
+
+typedef boost::rectangle_topology<std::minstd_rand> topology_t;
+typedef topology_t::point_type point_t;
+
+simplegraph_t graph;
+vertex_t vertices[MAX_NODES];
+std::vector<point_t> position_map;
+std::minstd_rand random_gen;
+topology_t topo(random_gen, 0.0, 0.0, 500.0, 500.0);
+
+vertex_t sel0 = 0, sel1 = 0;
+bool b_sel0 = false, b_sel1 = false;
 
 void UpdateDrawFrame();
+void CalculateLayout();
 int main() {
 
-    for (int i = 0; i < 10; i++) {
-        auto vert = boost::add_vertex({i}, graph);
-    }
-    for (int i = 0; i < 9; i++) {
-        for (int j = i + 1; j < 10; j++) {
-        }
-    }
+    position_map.reserve(MAX_NODES);
 
-    boost::print_graph(graph, boost::get(&Vec::key, graph));
+    for (int i = 0; i < 10; i++) {
+        vertices[i] = boost::add_vertex({i}, graph);
+    }
+    position_map.resize(boost::num_vertices(graph));
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "basic window");
@@ -44,6 +63,7 @@ int main() {
     camera.target = Vector2{0.0f, 0.0f};
     camera.zoom = 2.0f;
 
+    CalculateLayout();
     EnableCursor();
 
 #if defined(PLATFORM_WEB)
@@ -62,7 +82,38 @@ int main() {
     return 0;
 }
 
-void CalculatePhysics(float_t dt) {}
+void CalculateLayout() {
+    auto position_map_property =
+        boost::make_iterator_property_map(position_map.begin(), boost::get(boost::vertex_index, graph));
+
+    boost::random_graph_layout(graph, position_map_property, topo);
+    boost::kamada_kawai_spring_layout(graph, position_map_property, boost::get(&Edge::weight, graph), topo,
+                                      boost::edge_length(100.0));
+}
+
+void CalculatePhysics(float_t dt, float_t ft) {
+    static float_t last = 0.0f;
+    if (ft - last > 1.0f) {
+        last = ft;
+        // boost::add_vertex(Vec{static_cast<int>(boost::num_vertices(graph) + 1)}, graph);
+        // position_map.push_back(point_t{});
+        // CalculateLayout();
+    }
+}
+
+bool get_vertex_by_cursor(Vector2 vert, vertex_t &vertex_out) // pass by value because this is cheap
+{
+    boost::graph_traits<simplegraph_t>::vertex_iterator i, end;
+    for (boost::tie(i, end) = boost::vertices(graph); i != end; i++) {
+        const auto &p = position_map[*i];
+        std::cout << Vector2Distance(vert, Vector2{static_cast<float>(p[0]), static_cast<float>(p[1])}) << std::endl;
+        if (Vector2Distance(vert, Vector2{static_cast<float>(p[0]), static_cast<float>(p[1])}) < CIRCLE_RADIUS) {
+            vertex_out = *i;
+            return true;
+        }
+    }
+    return false;
+}
 
 void UpdateDrawFrame() {
     float_t dt = GetFrameTime();
@@ -81,6 +132,8 @@ void UpdateDrawFrame() {
     }
 
     camera.zoom = Clamp(camera.zoom + GetMouseWheelMove() * 300.0f * dt, 2.0f, 1000.0f);
+    if (IsKeyPressed(KEY_T))
+        camera.target = Vector2{static_cast<float>(position_map[0][0]), static_cast<float>(position_map[0][1])};
 
     // This converts our mouse cursor (local relative to window)
     // to absolute coord with camera
@@ -95,10 +148,64 @@ void UpdateDrawFrame() {
         cursorPos = camera.target;
     }
 
-    CalculatePhysics(dt);
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        vertex_t vert_out;
+        if (!b_sel0) {
+            bool success = get_vertex_by_cursor(cursorPos, vert_out);
+            if (success) {
+                b_sel0 = true;
+                sel0 = vert_out;
+            }
+        } else if (!b_sel1) {
+            bool success = get_vertex_by_cursor(cursorPos, vert_out);
+            if (success && vert_out != sel0) {
+                b_sel1 = true;
+                sel1 = vert_out;
+            }
+        }
+    }
+
+    if (b_sel0 && b_sel1) {
+        boost::add_edge(sel0, sel1, graph);
+        CalculateLayout();
+        b_sel0 = b_sel1 = sel0 = sel1 = false;
+    }
+
+    CalculatePhysics(dt, ft);
     BeginDrawing();
     {
         BeginMode2D(camera);
+
+        // draw vertices
+        auto id_map = boost::get(boost::vertex_index, graph);
+        bool visited[MAX_NODES] = {false};
+        boost::graph_traits<simplegraph_t>::vertex_iterator i, end;
+        boost::graph_traits<simplegraph_t>::adjacency_iterator ai, a_end;
+
+        for (boost::tie(i, end) = boost::vertices(graph); i != end; ++i) {
+            const auto &p = position_map[*i];
+            for (boost::tie(ai, a_end) = boost::adjacent_vertices(*i, graph); ai != a_end; ++ai) {
+                if (ai == a_end)
+                    break;
+                if (visited[*ai]) {
+                    continue;
+                }
+                const auto &p2 = position_map[*ai];
+                DrawLine(static_cast<int>(p[0]), static_cast<int>(p[1]), static_cast<int>(p2[0]),
+                         static_cast<int>(p2[1]), RED);
+            }
+            visited[*i] = true;
+        }
+        for (boost::tie(i, end) = boost::vertices(graph); i != end; ++i) {
+            const auto &p = position_map[*i];
+            if ((b_sel0 && *i == sel0) || (b_sel1 && *i == sel1)) {
+                DrawCircle(static_cast<int>(p[0]), static_cast<int>(p[1]), CIRCLE_RADIUS, BLUE);
+            } else {
+                DrawCircle(static_cast<int>(p[0]), static_cast<int>(p[1]), CIRCLE_RADIUS, RED);
+            }
+            DrawText(std::to_string(boost::get(&Vec::key, graph)[*i]).c_str(), static_cast<int>(p[0]),
+                     static_cast<int>(p[1]), 18, BLACK);
+        }
         EndMode2D();
         DrawFPS(0, 0);
     }
